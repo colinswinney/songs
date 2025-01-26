fb8bc6d8a2f02b3113051c19877d5acfad467168
Colin Swinney <colinjswinney@gmail.com>
2025-01-26 22:33:43 +0100

    Add favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,22 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-	title: 'Songs',
-	description: 'A collection of songs.',
+	title: "Songs",
+	description: "A collection of songs.",
+	icons: {
+		icon: [
+			{
+				media: "(prefers-color-scheme: light)",
+				url: "/images/music-note-light.png",
+				href: "/images/music-note-light.png",
+			},
+			{
+				media: "(prefers-color-scheme: dark)",
+				url: "/images/music-note-dark.png",
+				href: "/images/music-note-dark.png",
+			},
+		],
+	},
 };
 
 export default function RootLayout({
